4e4de85f104d628a0f943ee65235e266eb5ccf6c
Bluemaster23 <crsitanblue@gmail.com>
2024-11-04 14:16:46 -0500

    cors modif

--- src/main/java/com/example/prueba/config/SecurityConfig.java
+++ src/main/java/com/example/prueba/config/SecurityConfig.java
@@ -39,8 +39,8 @@ public class SecurityConfig {
     @Bean
     public UrlBasedCorsConfigurationSource addCorsMappings() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://front-veci-fhd8.onrender.com","http://localhost:3000"));
-        config.setAllowedOriginPatterns(List.of("https://front-veci-fhd8.onrender.com","http://localhost:3000"));
+        config.setAllowedOrigins(List.of("front-veci-fhd8.onrender.com","https://front-veci-fhd8.onrender.com","http://localhost:3000"));
+        config.setAllowedOriginPatterns(List.of("front-veci-fhd8.onrender.com","https://front-veci-fhd8.onrender.com","http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
